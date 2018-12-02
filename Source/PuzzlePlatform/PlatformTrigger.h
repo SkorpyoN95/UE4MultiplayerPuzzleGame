3d2407c313f4487633368f41479242be832a3a32
Dominik Chojny <dominik.chojny@gmail.com>
2018-12-02 04:29:21 +0100

    Section 1. Lesson 13.

---
+++ Source/PuzzlePlatform/PlatformTrigger.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PlatformTrigger.generated.h"
+
+UCLASS()
+class PUZZLEPLATFORM_API APlatformTrigger : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APlatformTrigger();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(VisibleAnywhere) // Visible - because its pointer, not an object, and we dont want to mess with pointer, we want to change object this pointer point.
+	class UBoxComponent* TriggerVolume;
+	
+};
